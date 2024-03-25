57d4855ddf6d82aebe79c3828ac40e565ea1d28a
Blair-tony <152367164+Blair-tony@users.noreply.github.com>
2024-03-25 22:54:50 +0530

    Delete tasks/25032024/project directory

--- tasks/25032024/project/job/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import JobListing
-# Create your views here.
-from django.contrib.auth.models import User
-
-class CompanyProfile(models.Model):
-    user = models.OneToOneField(User, related_name='company_profile', on_delete=models.CASCADE)
-    # Other fields
--- tasks/25032024/project/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserProfile,Profile
-
-admin.site.register(UserProfile)
-admin.site.register(Profile)
