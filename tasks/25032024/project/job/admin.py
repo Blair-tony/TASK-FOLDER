229ff469c6043247d6f3f2bf3e82f8c24fcabbd2
Blair-tony <152367164+Blair-tony@users.noreply.github.com>
2024-03-26 21:31:57 +0530

    Delete tasks/25032024 directory

--- tasks/25032024/project/job/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import JobApplication,JobListing,CompanyProfile
-
-admin.site.register(JobApplication)
-admin.site.register(JobListing)
-admin.site.register(CompanyProfile)
--- tasks/25032024/project/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserProfile,Profile,CompanyProfile,JobApplication,Resume
-admin.site.register(UserProfile)
-# admin.site.register(CompanyProfile)
-admin.site.register(JobApplication)
-admin.site.register(Resume)
